b138afa8ad834368a335465f2148ff7d3ab16c67
asmfahim <asmfahim1@gmail.com>
2023-06-15 16:59:30 +0600

    fahim 15 June, 2023

--- lib/module/module_name/views/screen_name.dart
+++ lib/module/module_name/views/screen_name.dart
@@ -5,7 +5,98 @@ class DashboardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Container(child: Text("Project Setup"),),),);
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(50),
+        child: Row(
+          children: [
+            Expanded(
+              child: Container(
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/healthCare.jpg"),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: AssetImage('assets/images/images.png'),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Text('Hello Again!'),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text('Hello Again CHANGE THE QUALITY OF YOUR LIFE +8801999999999'),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Enter email',
+                        labelText: 'Email',
+                        labelStyle: TextStyle(
+                          color: Colors.black,
+                          fontSize: 14,
+                        ),
+                        border: OutlineInputBorder(),
+                        prefixIcon: Icon(Icons.email_outlined)
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Enter password',
+                        labelText: 'Password',
+                        labelStyle: TextStyle(
+                          color: Colors.black,
+                          fontSize: 14,
+                        ),
+                        border: OutlineInputBorder(),
+                        prefixIcon: Icon(Icons.lock_outline)
+                      ),
+                    ),
+                    SizedBox(height: 20,),
+                    Row(
+                      children: [
+                        Row(
+                          children: [
+                            Theme(
+                              data: Theme.of(context)
+                                  .copyWith(unselectedWidgetColor: Colors.black),
+                              child: Checkbox(
+                                value: true,
+                                activeColor: Colors.blue,
+                                checkColor: Colors.black,
+                                onChanged: (value) {},
+                              ),
+                            ),
+                            Text(
+                              "Incentive applicable",
+                            ),
+                          ],
+                        ),
+                        Text('Forget password ?')
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
 
